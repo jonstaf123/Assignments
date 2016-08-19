bf1c0e92ddebf9dca475206b8891374abbbe51cd
Jonathan Stafford <jondstafford@gmail.com>
2016-08-19 13:37:41 -0400

    added replay function to SimpleCraps

--- Day3/SimpleCraps.java
+++ Day3/SimpleCraps.java
@@ -1,29 +1,31 @@
 package game;
 
+import java.util.Scanner;
 
-	
-	class SimpleCraps {
+class SimpleCraps {
 		int value;
 		int point = 0;
-		int count = 1;
+		int count = 0;
 		boolean didWin, didLose;
-		
+		boolean replayYes = false;
+		boolean replayNo = false;
+				
 	public int getvalue() {
 		return value;
 	}
+	//sets the value of roll of dice and prints rolls
 	public void rollSimpleCraps() {
-		
-		
 		this.value = ((int) (Math.random() * (12) + 1));
 		System.out.printf("you rolled a %d\n", this.value );
 
 	}
+	
 		public void startRoll() {
 			while (!didWin || !didLose ) {
 				rollSimpleCraps();
+				
 			
-			
-			if (this.count++ == 1 ) {
+			if (this.count++ == 0 ) {
 				if (this.value == 7) {
 					didWin = true;
 					break;
@@ -35,6 +37,7 @@ package game;
 				}
 			}
 			 else {
+				 //roll point player loses
 				if (this.point == this.value) {
 					didWin = true;
 					break;
@@ -42,21 +45,43 @@ package game;
 					break;
 				}
 			}
+			
+			}
+		
+		}
+		
+
+	public static void main(String[] args) {
+		SimpleCraps simpleCraps = new SimpleCraps();
+		Scanner in =  new Scanner(System.in);
+		do {
+			simpleCraps.replayYes= false;
+		
+		simpleCraps.startRoll();
+		System.out.printf("Point: %d\n", simpleCraps.point);
+		System.out.printf("rolls: %d\n", simpleCraps.count);
+		if (simpleCraps.didWin == true) {
+			System.out.println("You won!");
+		} else {
+			System.out.println("Sorry you lost");
+		}
+		
+		
+		
+		System.out.print("To play again or to quit hit (Y/N):");
+		String answer = in.next();		
+		 if ( answer.equalsIgnoreCase("Y") ){
+			 simpleCraps.replayYes = true;		 
+		
+		} 
+		 
+		 } while (simpleCraps.replayYes);
+		
+		
+		
 		}
 	}
 
 	
 
-	public static void main(String[] args) {
-		SimpleCraps SimpleCraps = new SimpleCraps();
-		SimpleCraps.startRoll();
-		System.out.printf("Point: %d\n", SimpleCraps.point);
-		if (SimpleCraps.didWin == true) {
-			System.out.println("You won!");
-		} else {
-			System.out.println("Sorry you lost try again");
-		}
-	}
-
-}
 
