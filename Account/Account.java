e7b4d70e570704eccfa563cff5610564404b8e24
Jonathan Stafford <jondstafford@gmail.com>
2016-08-23 15:13:30 -0400

    Assignment 6

--- Account/Account.java
+++ Account/Account.java
@@ -1,37 +1,84 @@
 package ssa;
 
-public class Account {
-	int accountId;
-	String description;
-	double balance, deposit1, deposit2, deposit3, withdraw1, withdraw2, withdraw3, totalBalance;
-	
-	
-	
-	
-	double addDeposit (){
-		double addDeposit = deposit1 + deposit2 + deposit3 + totalBalance;
-		totalBalance = addDeposit;
-		return totalBalance;
-		
-		
-		
-		
-	}
-	
-	
-	 double subtractWithdraw(){
-		
-		double subtractWithdraw =  totalBalance - withdraw1 - withdraw2 - withdraw3;
-		totalBalance =subtractWithdraw;
-		return totalBalance;
-	
+//A bank account
+class Account {
+	int accountId; // unique identifier for the account
+	String description; // a user defined name for the account
+	double balance; // the amount in the account.
+
+	public int getAccountId() {
+		return accountId;
 	}
 
+	private void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
 
-	 double getBalance (){
-		double totalBalance = 0;
-		return totalBalance;
-	 }
-	 
+	public double getBalance() {
+		return balance;
+	}
 
+	private void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	// returns the account info as a string
+	void print() {
+		System.out.println("Account: id is " + accountId + ", desc is " + description + ", balance is " + balance);
+		  
+	}
+
+	// deposit into the account
+	double deposit(double amount) {
+		this.balance += amount;
+		return this.balance;
+	}
+
+	// withdraw from the account, but cannot go negative
+	double withdraw(double amount) {
+		
+		if (amount > balance) {
+			System.out.println("Insufficient funds!");
+		} else {
+			this.balance -= amount;
+			
+		}
+		return this.balance;
+	}
+
+	public double transferFrom(Account account, double amount) {
+		if (amount > account.getBalance()) {
+			System.out.println("Insufficient funds!");
+		} else {
+			 account.withdraw(amount);
+			 this.deposit(amount);
+		}
+		return this.balance; 
+			
+		
+	}
+	public Account(int accountId, String description) {
+		this.accountId = accountId;
+		this.description = description;
+		this.balance = 0;
+		accountId = ((int) (Math.random() * (10) + 1));
+	}
+	public Account(String description) {
+		this.description = description;
+		this.balance = 0;
+		this.accountId = ((int) (Math.random() * (10) + 1));
+	}
+
+	public Account() {
+		this.balance = 0;
+		this.accountId = ((int) (Math.random() * (10) + 1));
+	}
 }
--- Account/Mainline.java
+++ Account/Mainline.java
@@ -3,85 +3,51 @@ package ssa;
 public class Mainline {
 
 	public static void main(String[] args) {
-		Account myCheckings = new Account();
-		myCheckings.accountId = 100;
-		myCheckings.description ="My personal checking account";
-		myCheckings.balance = 500.00;
-		myCheckings.deposit1 = 200;
-		myCheckings.withdraw1 = 600.00;
-		
-		
-		System.out.println("Account ID: " + myCheckings.accountId + " Account description: " + myCheckings.description
-				+ "Account Balance: " + myCheckings.balance);
-		myCheckings.totalBalance = myCheckings.balance; 
-		myCheckings.addDeposit();
-		System.out.println("Account ID: " + myCheckings.accountId + " Account description: " + myCheckings.description
-				+ "Account Balance: " + myCheckings.totalBalance);
-		myCheckings.subtractWithdraw();
-		System.out.println("Account ID: " + myCheckings.accountId + " Account description: " + myCheckings.description
-				+ "Account Balance: " + myCheckings.totalBalance);
-		//myCheckings.getBalance();
-		myCheckings.deposit2 = 100;
-		myCheckings.deposit1 = 0;
-		myCheckings.addDeposit();
-		System.out.println("Account ID: " + myCheckings.accountId + " Account description: " + myCheckings.description
-				+ "Account Balance: " + myCheckings.totalBalance);
-		
-		//myCheckings.getBalance();
-		myCheckings.withdraw2 = 300;
-		myCheckings.withdraw1 = 0;
-			
-		if (myCheckings.totalBalance < 0) {
-			myCheckings.subtractWithdraw();
-			System.out.println("Account ID: " + myCheckings.accountId + " Account description: " + myCheckings.description
-				+ "Account Balance: " + myCheckings.totalBalance);
-		}else {
-			System.out.println("Insufficient funds!");
-		}
-		
-		//myCheckings.getBalance();
-		myCheckings.withdraw3 = 200;
-		myCheckings.withdraw2 = 0;
-		myCheckings.withdraw1 = 0;
-		myCheckings.subtractWithdraw();
-		if (myCheckings.totalBalance >= 0) {
-			System.out.println("Account ID: " + myCheckings.accountId + " Account description: " + myCheckings.description
-				+ "Account Balance: " + myCheckings.totalBalance);
-		}else {
-		System.out.println("Insufficient funds!");
-		}
-		System.out.println("");
-	
-	
-		Account mySavings = new Account();
-		mySavings.accountId = 200;
-		mySavings.description ="My personal savings account";
-		mySavings.balance = 1000.00;
-		mySavings.deposit1 = 200;
-		mySavings.withdraw1 = 750.00;
-		mySavings.totalBalance = mySavings.balance;
-	
-	
-		mySavings.subtractWithdraw();
-		System.out.println("Account ID: " + mySavings.accountId + " Account description: " + mySavings.description
-				+ "Account Balance: " + mySavings.totalBalance);
-		mySavings.withdraw2 = 250;
-		mySavings.withdraw1 = 0;
-		mySavings.subtractWithdraw();
-		
-		System.out.println("Account ID: " + mySavings.accountId + " Account description: " + mySavings.description
-				+ "Account Balance: " + mySavings.totalBalance);
-	mySavings.addDeposit();
-	System.out.println("Account ID: " + mySavings.accountId + " Account description: " + mySavings.description
-			+ "Account Balance: " + mySavings.totalBalance);
-	
-	
-	
-	
-	
-	
-	
-	
+		Account checking1 = new Account();
+		checking1.setDescription("My personal checking account");
+		checking1.balance = 800.00;
+		checking1.print();
+		checking1.deposit(300.00);
+		checking1.print();
+		checking1.withdraw(699.99);
+		checking1.print();
+		checking1.deposit(149.99);
+		checking1.print();
+		checking1.withdraw(950.00); // this should fail!
+		checking1.print();
+		checking1.withdraw(200.00);
+		checking1.print(); // balance should be 350
+		//
+		Account savings1 = new Account("My personal savings account");
+		savings1.deposit(1000.00);
+		savings1.print();
+		savings1.deposit(400.00);
+		savings1.print();
+		savings1.withdraw(750.00);
+		savings1.print();
+		savings1.withdraw(250.00);
+		savings1.print();
+		savings1.deposit(650.00);
+		savings1.print(); // balance should be 1050.00
+		// the transfer
+		checking1.transferFrom(savings1, 100.00); // transfer 100 from savings to checking
+		// another account
+		Account checking2 = new Account("My slush fund checking account");
+		checking2.deposit(300.00);
+		checking2.print();
+		checking2.withdraw(150.00);
+		checking2.print();
+		checking2.deposit(250.00);
+		checking2.print();
+
+		// more to come. Will be updated during the lab...*
+
+		// print out all account balances
+		checking1.print();
+		checking2.print();
+		savings1.print();
+
 	}
 }
-		
+
+// int id - previously
